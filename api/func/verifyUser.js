98ec4e7fb079b3a1a46379609dc5eb8ace99c793
AmitSutradhar001 <sutradharamit001@gmail.com>
2024-09-11 23:22:22 +0530

    verify log

--- api/func/verifyUser.js
+++ api/func/verifyUser.js
@@ -2,8 +2,11 @@ import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, _, next) => {
   const token = req.cookies.asblog_token;
+
   console.log("token::-> ", token);
   console.log("asblog_token::-> ", req.cookies.asblog_token);
+  console.log("all cookies::->", req.cookies);
+  console.log("req::-> ", req);
 
   if (!token) {
     return next({
